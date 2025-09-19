b3196d7aeaa38aedff2958025c57447f9cb3625c
mmstf <mustafobey99@gmail.com>
2025-09-19 22:51:15 +0500

    init commit

---
+++ vars/deployToMaster.groovy
@@ -0,0 +1,24 @@
+def call() {
+    withCredentials(
+        [
+            usernamePassword(
+            credentialsId: 'docker-pat',
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+    )]) {
+            sh '''
+                echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+                docker pull docker.io/mmstf/mainapp:v1.0
+            '''
+
+            sh """
+                OLD_CONTAINER=\$(docker ps -q --filter name=main-app)
+                if [ "\$OLD_CONTAINER" ]; then
+                    docker stop \$OLD_CONTAINER
+                    docker rm \$OLD_CONTAINER
+                fi
+            """
+
+            sh "docker run -d --name main-app -p 3000:3000 mmstf/mainapp:v1.0"
+        }
+}
